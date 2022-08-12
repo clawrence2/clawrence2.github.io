5c13a2e239686ded7e638e5cd5be96185a332013
Collin Lawrence <hicollin@gmail.com>
2022-08-12 14:40:18 -0400

    add resolution test pages and reorganize site

---
+++ scripts/resolution-test.js
@@ -0,0 +1,7 @@
+console.log("chicken")
+
+// const devicePR = window.devicePixelRatio;
+// console.log(devicePR);
+// const DPRspan = document.getElementById('DPR-span');
+// DPRspan.innerHTML = devicePR;
+// console.log(devicePR);
