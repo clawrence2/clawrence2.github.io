1cc1e6fb366d2c2f57dfa597808e350da7d04f45
Collin Lawrence <hicollin@gmail.com>
2022-09-07 11:56:53 -0400

    add test shell for testing Oshin

---
+++ test-shell/scripts/main.js
@@ -0,0 +1,52 @@
+
+// BURGER TOGGLE
+document.addEventListener('DOMContentLoaded', nav);
+function nav() {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.main-nav');
+    const flyout = document.querySelector('.flyout');
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('show');
+        flyout.classList.toggle('show');
+
+    })
+}
+
+//SCROLL TOGGLE
+const navFixed = document.querySelector('nav');
+navFixed.addEventListener('click', showNav);
+
+
+function hideNav(){
+    navFixed.classList.toggle('scroll-down');
+    console.log("fired");
+}
+
+function showNav(){
+    navFixed.classList.toggle('scroll-up');
+}
+
+const scrollUp = "scroll-up";
+const scrollDown = "scroll-down";
+let lastScroll = 0;
+window.addEventListener("scroll", () => {
+    const currentScroll = window.pageYOffset;
+    if (currentScroll <= 0) {
+      navFixed.classList.remove(scrollUp);
+      return;
+    }
+   
+    if (currentScroll > lastScroll && !navFixed.classList.contains(scrollDown)) {
+      // down
+      navFixed.classList.remove(scrollUp);
+      navFixed.classList.add(scrollDown);
+    } else if (
+      currentScroll < lastScroll &&
+      navFixed.classList.contains(scrollDown)
+    ) {
+      // up
+      navFixed.classList.remove(scrollDown);
+      navFixed.classList.add(scrollUp);
+    }
+    lastScroll = currentScroll;
+  });
