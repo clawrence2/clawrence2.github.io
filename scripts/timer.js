4c36f4134ed70d45fc585a4b4603176fa738c41a
Collin Lawrence <hicollin@gmail.com>
2022-09-18 16:56:03 -0400

    add second timer

--- scripts/timer.js
+++ scripts/timer.js
@@ -1,5 +1,5 @@
 const oDate = new Date("Aug 18, 2022 23:00:00").getTime();
-const sDate = new Date("Aug 25, 2022 00:30:00").getTime();
+const sDate = new Date("Sep 18, 2022 16:00:00").getTime();
 const oTimer = document.querySelector("#oTimer");
 const sTimer = document.querySelector("#sTimer");
 
@@ -14,5 +14,17 @@ function getTimeO() {
     oTimer.textContent = oTime;
 }
 
+function getTimeS() {
+    let today = new Date().getTime();
+    let distance = today - sDate;
+    let sDays = Math.floor(distance / (1000 * 60 * 60 * 24));
+    let sHours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let sMinutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    let sSeconds = Math.floor((distance % (1000 * 60)) / 1000);
+    let sTime = sDays + "d " + sHours + "h " + sMinutes + "m " + sSeconds + "s ";
+    sTimer.textContent = sTime;
+}
+
 
 setInterval(getTimeO,1000);
+setInterval(getTimeS,1000);
