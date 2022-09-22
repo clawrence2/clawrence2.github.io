88f16f96c1a4e189621643f12acd0d4f6ac5d04f
Collin Lawrence <hicollin@gmail.com>
2022-09-22 16:51:45 -0400

    update new tracker to use tailwind

---
+++ tailwind/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily:{
+        spicy:['Bungee Spice'],
+        fredoka:['Fredoka One']
+      },
+    },
+  },
+  plugins: [],
+}
