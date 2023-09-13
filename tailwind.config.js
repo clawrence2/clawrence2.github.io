c07d4844331005cfce9c8ce815c1931383e48f7b
Collin Lawrence <hicollin@gmail.com>
2023-09-13 18:58:28 -0400

    speed up animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,10 +10,15 @@ module.exports = {
         scrollage: {
           '0%': { transform: 'translateX(0)' },
           '100%': { transform: 'translateX(-100%)' },
+        },
+        scrollageFast: {
+          '0%': { transform: 'translateX(0)' },
+          '100%': { transform: 'translateX(-100%)' },
         }
       },
       animation:{
-        scrollage: 'scrollage 60s linear infinite',
+        scrollage: 'scrollage 45s linear infinite',
+        scrollageFast: 'scrollage 20s linear infinite',
       },
       screens:{
         'my10' : '1000px',
