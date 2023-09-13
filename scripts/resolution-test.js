3323115c7ee01eba05974f6fc3574e517879515f
Collin Lawrence <hicollin@gmail.com>
2023-09-13 15:58:46 -0400

    cutover commit

--- scripts/resolution-test.js
+++
@@ -1,7 +0,0 @@
-console.log("chicken")
-
-// const devicePR = window.devicePixelRatio;
-// console.log(devicePR);
-// const DPRspan = document.getElementById('DPR-span');
-// DPRspan.innerHTML = devicePR;
-// console.log(devicePR);
